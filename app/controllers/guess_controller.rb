202b72a649c5202fc5ca874ae5f2a8a2560301e0
Shirley Shaw <shirleyshaw.is@gmail.com>
2015-02-05 15:45:47 -0800

    added the heroku clear active connections fix

--- app/controllers/guess_controller.rb
+++ app/controllers/guess_controller.rb
@@ -1,5 +1,8 @@
 class GuessController < ApplicationController
-
+  	after do
+	  ActiveRecord::Base.clear_active_connections!
+	end
+	
 	def index
     if ((params[:height].present?) && (params[:height].to_i != 0) && (params[:weight].to_i != 0))
     	@query_height = params[:height].to_i
--- app/controllers/thanks_controller.rb
+++ app/controllers/thanks_controller.rb
@@ -1,2 +1,5 @@
 class ThanksController < ApplicationController
+  after do
+	  ActiveRecord::Base.clear_active_connections!
+	end
 end
