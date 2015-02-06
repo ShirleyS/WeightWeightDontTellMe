52c092f5bc9d28ec1681c056c0a345443df2a00e
Shirley Shaw <shirleyshaw.is@gmail.com>
2015-02-05 16:17:26 -0800

    h fix

--- db/migrate/20150204201833_create_guesses.rb
+++ db/migrate/20150204201833_create_guesses.rb
@@ -1,22 +1,6 @@
 class CreateGuesses < ActiveRecord::Migration
 
-
-def with_connection(&block)
-  ActiveRecord::Base.connection_pool.with_connection do
-    yield block
-  end
-	ensure
-	  ActiveRecord::Base.clear_active_connections!
-	  ActiveRecord::Base.connection.close
-end
-
   def change
-
-		after do
-	    with_connection do 
-	  	do_things_with_activerecord_db_and_get_some_parameters()   
-			end
-	  end
     
     create_table :guesses do |t|
     	t.integer	:height
